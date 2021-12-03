01fbddc32cb7581cd0a23df7e5836743e0986a2f
Sanskar Agrawal <sanskar10100@gmail.com>
2021-12-04 00:41:09 +0530

    Added navigation action and fab in HomeFragment.kt

--- app/src/main/java/dev/sanskar/transactions/ui/home/HomeFragment.kt
+++ app/src/main/java/dev/sanskar/transactions/ui/home/HomeFragment.kt
@@ -1,7 +1,31 @@
 package dev.sanskar.transactions.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import dev.sanskar.transactions.R
+import dev.sanskar.transactions.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabAddTransaction.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_addTransactionFragment)
+        }
+    }
 }
