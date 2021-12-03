2b2fb8fef64f825bc885e6e5f29373f75608d847
Sanskar Agrawal <sanskar10100@gmail.com>
2021-12-04 02:01:53 +0530

    Add model observer and refresh in HomeFragment

--- app/src/main/java/dev/sanskar/transactions/ui/home/HomeFragment.kt
+++ app/src/main/java/dev/sanskar/transactions/ui/home/HomeFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import dev.sanskar.transactions.R
 import dev.sanskar.transactions.databinding.FragmentHomeBinding
+import dev.sanskar.transactions.ui.model.MainViewModel
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private val model by activityViewModels<MainViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +29,10 @@ class HomeFragment : Fragment() {
 
         binding.fabAddTransaction.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_addTransactionFragment)
+        }
+
+        model.transactions.observe(viewLifecycleOwner) {
+            binding.listTransactions.adapter = TransactionsListAdapter(it)
         }
     }
 }
