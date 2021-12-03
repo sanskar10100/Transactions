c319747a7d651b67d154cdcaf475bddafcfac294
Sanskar Agrawal <sanskar10100@gmail.com>
2021-12-04 01:26:09 +0530

    Updated schema version

--- app/src/main/java/dev/sanskar/transactions/data/TransactionDatabase.kt
+++ app/src/main/java/dev/sanskar/transactions/data/TransactionDatabase.kt
@@ -3,7 +3,7 @@ package dev.sanskar.transactions.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Transaction::class], version = 1)
+@Database(entities = [Transaction::class], version = 2)
 abstract class TransactionDatabase : RoomDatabase() {
     abstract fun transactionDao(): TransactionDao
 }
