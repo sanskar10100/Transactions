1e07e3c8a38bee4faffafcce2d09994f8faa6eee
Sanskar Agrawal <sanskar10100@gmail.com>
2021-12-04 01:25:44 +0530

    Removed unnecessary column info from entities

--- app/src/main/java/dev/sanskar/transactions/data/Transaction.kt
+++ app/src/main/java/dev/sanskar/transactions/data/Transaction.kt
@@ -1,15 +1,14 @@
 package dev.sanskar.transactions.data
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
 data class Transaction(
     @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "amount") val amount: Int,
-    @ColumnInfo(name = "added_at") val timestamp: Long,
-    @ColumnInfo(name = "is_expense") val isExpense: Boolean,
-    @ColumnInfo(name = "description") val description: String,
-    @ColumnInfo(name = "is_digital") val isDigital: Boolean
+    val amount: Int,
+    val timestamp: Long,
+    val isExpense: Boolean,
+    val description: String,
+    val isDigital: Boolean
 )
