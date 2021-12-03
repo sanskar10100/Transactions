dd261a30afbde9fa8393b048377d922be8cd3fa2
Sanskar Agrawal <sanskar10100@gmail.com>
2021-12-04 00:53:28 +0530

    Add missing RoomDatabase inheritance

--- app/src/main/java/dev/sanskar/transactions/data/TransactionDatabase.kt
+++ app/src/main/java/dev/sanskar/transactions/data/TransactionDatabase.kt
@@ -1,8 +1,9 @@
 package dev.sanskar.transactions.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [Transaction::class], version = 1)
-abstract class TransactionDatabase {
+abstract class TransactionDatabase : RoomDatabase() {
     abstract fun transactionDao(): TransactionDao
 }
