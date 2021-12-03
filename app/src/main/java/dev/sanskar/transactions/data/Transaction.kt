d15d448c07c20426f6b1b7cbdc62775d7dd3bdda
Sanskar Agrawal <sanskar10100@gmail.com>
2021-12-04 00:53:02 +0530

    Added autoGenerate attribute to primary key

--- app/src/main/java/dev/sanskar/transactions/data/Transaction.kt
+++ app/src/main/java/dev/sanskar/transactions/data/Transaction.kt
@@ -6,10 +6,10 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Transaction(
-    @PrimaryKey val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "amount") val amount: Int,
     @ColumnInfo(name = "added_at") val timestamp: Long,
     @ColumnInfo(name = "is_expense") val isExpense: Boolean,
     @ColumnInfo(name = "description") val description: String,
-    @ColumnInfo(name = "is_Digital") val isDigital: Boolean
+    @ColumnInfo(name = "is_digital") val isDigital: Boolean
 )
