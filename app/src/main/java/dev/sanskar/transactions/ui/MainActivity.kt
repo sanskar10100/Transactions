cac172525c032bde21896f6a4152c440a893deae
Sanskar Agrawal <sanskar10100@gmail.com>
2021-12-03 23:51:01 +0530

    Add standard empty transactions project

---
+++ app/src/main/java/dev/sanskar/transactions/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.sanskar.transactions.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.sanskar.transactions.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
