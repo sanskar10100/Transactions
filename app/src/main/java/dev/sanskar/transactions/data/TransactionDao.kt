0ec1fedd23c7dfd20049cdbb6029bd63ac6635ed
Sanskar Agrawal <sanskar10100@gmail.com>
2021-12-10 12:37:05 +0530

    Added default animations and transaction dao methods

--- app/src/main/java/dev/sanskar/transactions/data/TransactionDao.kt
+++ app/src/main/java/dev/sanskar/transactions/data/TransactionDao.kt
@@ -12,6 +12,24 @@ interface TransactionDao {
     @Query("SELECT * FROM `transaction` ORDER BY timestamp")
     fun getAllTransactions(): LiveData<List<Transaction>>
 
+    @Query("SELECT SUM(amount) FROM `transaction` WHERE isExpense = 1 AND isDigital = 0")
+    suspend fun getTotalCashExpenses(): Int
+
+    @Query("SELECT SUM(amount) FROM `transaction` WHERE isExpense = 1 AND isDigital = 1")
+    suspend fun getTotalDigitalExpenses(): Int
+
+    @Query("SELECT SUM(amount) FROM `transaction` WHERE isExpense = 0 AND isDigital = 0")
+    suspend fun getTotalCashIncome(): Int
+
+    @Query("SELECT SUM(amount) FROM `transaction` WHERE isExpense = 0 and isDigital = 1")
+    suspend fun getTotalDigitalIncome(): Int
+
+    @Query("SELECT SUM(amount) FROM `transaction` WHERE isExpense = 0")
+    suspend fun getTotalIncome(): Int
+
+    @Query("SELECT SUM(amount) FROM `transaction` WHERE isExpense = 1")
+    suspend fun getTotalExpenses(): Int
+
     @Update
     suspend fun updateTransaction(transactions: Transaction)
 
@@ -20,4 +38,5 @@ interface TransactionDao {
 
     @Query("DELETE FROM `transaction`")
     suspend fun clearTransactions()
+
 }
