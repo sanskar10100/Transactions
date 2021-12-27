65256e476f6f6af537ae7c78c36972033937ceaa
Sanskar Agrawal <sanskar10100@gmail.com>
2021-12-27 22:50:54 +0530

    Add medium exchange (cash to digital and vice-versa) and updated build version

---
+++ app/src/main/java/dev/sanskar/transactions/ui/exchange/MediumExchangeFragment.kt
@@ -0,0 +1,64 @@
+package dev.sanskar.transactions.ui.exchange
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dev.sanskar.transactions.databinding.FragmentMediumExchangeBinding
+import dev.sanskar.transactions.getText
+import dev.sanskar.transactions.ui.model.MainViewModel
+
+class MediumExchangeFragment : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentMediumExchangeBinding
+    private val model by activityViewModels<MainViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMediumExchangeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.chipCashSource.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) binding.chipDigitalDestination.isChecked = true
+        }
+        binding.chipDigitalSource.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) binding.chipCashDestination.isChecked = true
+        }
+        binding.chipCashDestination.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) binding.chipDigitalSource.isChecked = true
+        }
+        binding.chipDigitalDestination.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) binding.chipCashSource.isChecked = true
+        }
+
+        binding.chipCashSource.isChecked = true
+
+
+        binding.buttonAdd.setOnClickListener {
+            if (binding.textFieldAmount.getText().isEmpty()) {
+                binding.textFieldAmount.error = "Amount cannot be empty!"
+                return@setOnClickListener
+            } else {
+                val amount = binding.textFieldAmount.getText().toInt();
+                if (binding.chipCashSource.isChecked) {
+                    // Remove from cash and add to digital
+                    model.addTransaction(amount, "Currency Medium Exchange", System.currentTimeMillis(), false, true)
+                    model.addTransaction(amount, "Currency Medium Exchange", System.currentTimeMillis(), true, false)
+                } else if (binding.chipDigitalSource.isChecked) {
+                    // Remove from digital and add to cash
+                    model.addTransaction(amount, "Currency Medium Exchange", System.currentTimeMillis(), true, true)
+                    model.addTransaction(amount, "Currency Medium Exchange", System.currentTimeMillis(), false, false)
+                }
+                dialog?.dismiss()
+            }
+        }
+    }
+}
