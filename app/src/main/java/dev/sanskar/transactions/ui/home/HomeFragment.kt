26e8c7921d9ec83e772cddc9e521a493430be90a
Sanskar Agrawal <sanskar10100@gmail.com>
2022-01-05 20:52:21 +0530

    Added ability to send feedback to the app dev

--- app/src/main/java/dev/sanskar/transactions/ui/home/HomeFragment.kt
+++ app/src/main/java/dev/sanskar/transactions/ui/home/HomeFragment.kt
@@ -1,8 +1,12 @@
 package dev.sanskar.transactions.ui.home
 
 import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -13,7 +17,9 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dev.sanskar.transactions.R
 import dev.sanskar.transactions.databinding.FragmentHomeBinding
 import dev.sanskar.transactions.ui.model.MainViewModel
+import kotlin.math.log
 
+private const val TAG = "HomeFragment"
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private val model by activityViewModels<MainViewModel>()
@@ -45,21 +51,48 @@ class HomeFragment : Fragment() {
             R.id.action_clear_transactions -> {
                 // Clear transactions on dialog confirmation
                 MaterialAlertDialogBuilder(requireContext())
-                        .setTitle("Are you sure?")
-                        .setMessage("Are you sure you want to delete all transactions to date?")
-                        .setPositiveButton("Yes") { _, _ ->
-                            model.clearTransactions()
-                        }
-                        .setNegativeButton("No") { dialog, _ ->
-                            dialog.dismiss()
-                        }
-                        .show()
+                    .setTitle("Are you sure?")
+                    .setMessage("Are you sure you want to delete all transactions to date?")
+                    .setPositiveButton("Yes") { _, _ ->
+                        model.clearTransactions()
+                    }
+                    .setNegativeButton("No") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
             }
             R.id.action_dashboard -> {
                 findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToDashboardFragment())
             }
             R.id.action_exchange_medium -> {
                 findNavController().navigate(R.id.action_homeFragment_to_mediumExchangeFragment)
+            }
+            R.id.action_send_feedback -> {
+                MaterialAlertDialogBuilder(requireContext())
+                    .setItems(arrayOf("GitHub", "Gmail")) { _, which ->
+                        Log.d(TAG, "onOptionsItemSelected: $which")
+                        when (which) {
+                            0 -> {
+                                Log.d(
+                                    TAG,
+                                    "onOptionsItemSelected: Attempting to create and launch a custom tab"
+                                )
+                            CustomTabsIntent.Builder()
+                                .build()
+                                .launchUrl(requireContext(), Uri.parse("https://github.com/sanskar10100/Transactions/issues/new"))
+                            }
+                            1 -> {
+                                val intent = Intent(Intent.ACTION_SENDTO).apply {
+                                    data = Uri.parse("mailto:")
+                                    putExtra(Intent.EXTRA_EMAIL, arrayOf("sanskar10100@gmail.com"))
+                                    putExtra(Intent.EXTRA_SUBJECT, "Feedback - Transactions")
+                                }
+                                startActivity(Intent.createChooser(intent, "Send Feedback"))
+                            }
+                        }
+                    }
+                    .create()
+                    .show()
             }
         }
 
