da768277913b0873ed418f291f7b366907dccf40
Sanskar Agrawal <sanskar10100@gmail.com>
2022-02-04 23:25:47 +0530

    Add View Only option navigation and selection handling

--- app/src/main/java/dev/sanskar/transactions/utils.kt
+++ app/src/main/java/dev/sanskar/transactions/utils.kt
@@ -8,6 +8,9 @@ import com.google.android.material.textfield.TextInputLayout
 import java.text.SimpleDateFormat
 import java.util.*
 
+const val VIEW_OPTIONS_REQUEST_KEY = "view_options_request_key"
+const val KEY_SELECTED_VIEW_OPTION = "selected"
+
 fun Long.asFormattedDateTime() : String {
     return SimpleDateFormat(
         "dd/MM/yy hh:mm aaa",
